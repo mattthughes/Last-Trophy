cccab0e53ba8776e3d1cedfa0ee132efc7b23f2a
Matthew Hughes <matt.h17@yahoo.com>
2024-02-23 16:20:06 +0000

    Created game model

--- last_trophy/settings.py
+++ last_trophy/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-mattthughes-lasttrophy-lpvrdyshgod.ws-eu108.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-mattthughes-lasttrophy-5umka5gr408.ws-eu108.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
@@ -88,6 +88,12 @@ WSGI_APPLICATION = 'last_trophy.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
+
 
 
 # Password validation
