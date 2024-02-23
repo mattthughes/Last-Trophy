b2f7a4c7986febcd5431a226a279772e2d788fbe
Matthew Hughes <matt.h17@yahoo.com>
2024-02-23 17:28:19 +0000

    Added trophies model

--- trophy_hunter/admin.py
+++ trophy_hunter/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-
+from .models import Game, Trophies
 
 # Register your models here.
+admin.site.register(Game)
+admin.site.register(Trophies)
--- trophy_hunter/models.py
+++ trophy_hunter/models.py
@@ -2,4 +2,31 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+    trophies = models.CharField(max_length=4)
+    hours = models.CharField(max_length=4)
 
+    def __str__(self):
+        return f"{self.title}"
+    
+class Trophies(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    game = models.ForeignKey(
+        Game, on_delete=models.CASCADE, related_name='achievements')
+    author = models.ForeignKey(
+            User, on_delete=models.CASCADE
+        )
+    description = models.CharField(max_length=200)
+    difficulty = models.CharField(max_length=200)
+    rarity = models.CharField(max_length=10)
+
+    class Meta:
+        ordering = ["-rarity"]
+
+    def __str__(self):
+        return f" {self.title} {self.description}"
