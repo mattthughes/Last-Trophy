d4e2bf8ed0dd69fca239d6ffbaa816f5699e2566
Matthew Hughes <matt.h17@yahoo.com>
2024-02-23 16:38:56 +0000

    Added new database

--- last_trophy/settings.py
+++ last_trophy/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-mattthughes-lasttrophy-5umka5gr408.ws-eu108.gitpod.io', '.herokuapp.com']
 
--- trophy_hunter/models.py
+++ trophy_hunter/models.py
@@ -2,17 +2,4 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Game(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.CharField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE
-    )
-    trophies = models.CharField(max_length=4)
-    hours = models.CharField(max_length=4)
-    image = models.ImageField(upload_to='images/', blank=True)
-
-    def __str__(self):
-        return f"{self.title}"
-
 
