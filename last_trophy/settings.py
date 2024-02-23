aba34aa8c610b18e814f15e968bc68fbb46f2910
Matthew Hughes <matt.h17@yahoo.com>
2024-02-23 09:26:36 +0000

    Added secret key for deployment

--- last_trophy/settings.py
+++ last_trophy/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0u&8!y^kx4r2zn79qev6s#r3=2^3(o!90jn=croj6__hz**5-a'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
