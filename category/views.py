c15ac85c0dfa96f907a2b89945244443632922f6
Matthew Hughes <matt.h17@yahoo.com>
2024-03-02 23:43:01 +0000

    Added category page links and fixed view bug

--- category/views.py
+++ category/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views import generic
+from trophy_hunter.models import Categories
 # Create your views here.
+
+def CategoryList(request):
+    categories = Categories.objects.all()
+
+    return render(
+        request,
+        "category.html",
+        {
+            "categories": categories,
+        },
+    )
--- last_trophy/urls.py
+++ last_trophy/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("trophy_hunter.urls"), name="trophy_hunter-urls" )
+    path("category/", include("category.urls"), name="category-urls"),
+    path("", include("trophy_hunter.urls"), name="trophy_hunter-urls" ),
+    
 ]
