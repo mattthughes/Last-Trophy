9ca0aed310fe5d571e6d37c3e4c63ad75c1b3509
Matthew Hughes <matt.h17@yahoo.com>
2024-03-02 21:27:16 +0000

    Created genre database model

--- trophy_hunter/admin.py
+++ trophy_hunter/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Game, Trophy
-
+from .models import Game, Trophy, Genre
 # Register your models here.
 admin.site.register(Game)
 admin.site.register(Trophy)
+admin.site.register(Genre)
--- trophy_hunter/models.py
+++ trophy_hunter/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Game(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -32,3 +33,8 @@ class Trophy(models.Model):
 
     def __str__(self):
         return f" {self.title} {self.description}"
+
+class Genre(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    
