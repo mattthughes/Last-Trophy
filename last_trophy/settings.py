d23792c66fb6392dd52847b3f1ce63815267e7d5
Matthew Hughes <matt.h17@yahoo.com>
2024-03-10 19:32:04 +0000

    Deployed latest project to heroku

--- last_trophy/settings.py
+++ last_trophy/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-mattthughes-lasttrophy-c4ulsmm76dg.ws-eu108.gitpod.io', '.herokuapp.com']
 
