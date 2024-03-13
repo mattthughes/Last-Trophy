5b5c5765f5040c7a65a50605872fbb86a72157e9
Matthew Hughes <matt.h17@yahoo.com>
2024-03-13 20:36:32 +0000

    Added mobile, desktop styles for trophy section

--- last_trophy/settings.py
+++ last_trophy/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-mattthughes-lasttrophy-fpyxbgjkn4w.ws-eu108.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-mattthughes-lasttrophy-fpyxbgjkn4w.ws-eu110.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
