db5af22c976720d6f067fe99709faedc4715006c
Matthew Hughes <matt.h17@yahoo.com>
2024-03-14 21:17:49 +0000

    Added guides model

--- trophy_hunter/admin.py
+++ trophy_hunter/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Game, Trophies
+from .models import Game, Trophies, Guide
 # Register your models here.
 admin.site.register(Game)
 admin.site.register(Trophies)
+admin.site.register(Guide)
--- trophy_hunter/models.py
+++ trophy_hunter/models.py
@@ -42,3 +42,20 @@ class Trophies(models.Model):
     def __str__(self):
         return f" {self.title} {self.description}"
 
+class Guide(models.Model):
+    game = models.ForeignKey(
+        Game, on_delete=models.CASCADE, related_name='guides'
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE)
+    trophy = models.ForeignKey(
+        Trophies, on_delete=models.CASCADE
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f" {self.body} {self.author}"
+
+
