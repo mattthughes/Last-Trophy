0ac781499ab62d72f0b2b8cfff1bc4425d55c3f8
Matthew Hughes <matt.h17@yahoo.com>
2024-03-15 09:21:34 +0000

    Added bootstrap flex styles to footer

--- category/filters.py
+++ category/filters.py
@@ -3,7 +3,6 @@ from trophy_hunter.models import Game
 
 class GameFilter(django_filters.FilterSet):
     trophy_count = django_filters.RangeFilter()
-    hours = django_filters.RangeFilter()
     class Meta:
         model = Game
         fields = {
