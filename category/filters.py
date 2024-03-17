26fcfbcee74664ad3c4e9cc848f2491716462a10
Matthew Hughes <matt.h17@yahoo.com>
2024-03-17 21:30:13 +0000

    Added new styling for genre form for all screen sizes

--- category/filters.py
+++ category/filters.py
@@ -2,7 +2,8 @@ import django_filters
 from trophy_hunter.models import Game
 
 class GameFilter(django_filters.FilterSet):
-    trophy_count = django_filters.RangeFilter()
+    trophies = django_filters.RangeFilter()
+    hours = django_filters.RangeFilter()
     class Meta:
         model = Game
         fields = {
