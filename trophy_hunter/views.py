ce0ef6eda1fbfe15343c774c1de54528092338de
Matthew Hughes <matt.h17@yahoo.com>
2024-03-26 20:33:24 +0000

    Added logout styling for all devices

--- trophy_hunter/views.py
+++ trophy_hunter/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.http import HttpResponse
 from django.views import generic
 
+
 # Create your views here.
 def index(request):
     return render(
