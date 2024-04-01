0504e41e46fec4ac017fa57192a84d279985f41c
Matthew Hughes <matt.h17@yahoo.com>
2024-04-01 14:56:04 +0000

    Added favicon

--- last_trophy/urls.py
+++ last_trophy/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path("game", include("category.urls"), name="category-urls"),
     path("", include("trophy_hunter.urls"), name="trophy_hunter-urls"),
     path("guides", include("guides.urls"), name="guides-urls")
+    
 
 ]
