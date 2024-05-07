4b32f184b0b87bac6cc552428b78c8fec3f7bf4e
Matthew Hughes <matt.h17@yahoo.com>
2024-05-07 16:52:29 +0000

    Adjusted genre form filter to include a better description

--- category/filters.py
+++ category/filters.py
@@ -3,11 +3,11 @@ from trophy_hunter.models import Game
 
 
 class GameFilter(django_filters.FilterSet):
-    hours = django_filters.RangeFilter()
 
     class Meta:
         model = Game
         fields = {
             'title': ['icontains'],
             'genre': ['exact'],
+            'hours': ['lt', 'gt']
         }
