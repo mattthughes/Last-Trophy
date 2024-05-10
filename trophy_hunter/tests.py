a67abcfc8020ee3769b840534a091c5acf543b58
Matthew Hughes <matt.h17@yahoo.com>
2024-05-10 21:00:29 +0000

    Added trophy detail, guide detail tests

--- trophy_hunter/tests.py
+++ trophy_hunter/tests.py
@@ -44,7 +44,7 @@ class TestViews(TestCase):
         template and response code.
         """
         test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
-        test = Game.objects.create(
+        test_game = Game.objects.create(
             title='darksouls',
             slug = 'dark-souls',
             author= test_user1,
@@ -56,6 +56,83 @@ class TestViews(TestCase):
 
         )
 
-        response = self.client.get(reverse('game-detail', args=(test.slug,)))
+        response = self.client.get(reverse('game-detail', args=(test_game.slug,)))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'game_detail.html')
+    
+    def test_trophy_detail_view(self):
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_game = Game.objects.create(
+            title='darksouls',
+            slug = 'dark-souls',
+            author= test_user1,
+            genre= 'action',
+            trophy_count= 50,
+            hours = 60,
+            rating= 4.9
+
+
+        )
+
+        test_trophy = Trophies.objects.create(
+            title = 'trophy test',
+            slug = 'trophy-test',
+            game= test_game,
+            description= 'checking trophy test works',
+            difficulty = 1,
+            rarity = 10.4
+
+        )
+
+        response = self.client.get(reverse('trophy-detail', args=(test_trophy.slug,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'trophy_detail.html')
+    
+    def test_guide_detail_view(self):
+        """
+        This function is testing the guide
+        detail view creating the game, trophies
+        and the guide object in the test database
+        this is then returning the reverse guide view
+        with the argument being the guides primary key
+        this is then making sure the correct template
+        is being used and the correct response code is
+        being returned.
+        """
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_game = Game.objects.create(
+            title='darksouls',
+            slug = 'dark-souls',
+            author= test_user1,
+            genre= 'action',
+            trophy_count= 50,
+            hours = 60,
+            rating= 4.9
+
+
+        )
+
+        test_trophy = Trophies.objects.create(
+            title = 'trophy test',
+            slug = 'trophy-test',
+            game= test_game,
+            description= 'checking trophy test works',
+            difficulty = 1,
+            rarity = 10.4
+
+        )
+
+        test_guide = Guide.objects.create(
+            title= 'you did it',
+            author = test_user1,
+            trophy= test_trophy,
+            body='testing guide creation works',
+            approved= True,
+        )
+
+        response = self.client.get(reverse('guide-view', args=(test_guide.pk,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'guide_detail_view.html')
+
+
+    
